a44f2ea828dbfd290e2e233fc39f39e03e91c58d
Onkar Jadhav <omjadhav2610@gmail.com>
2023-09-19 11:45:55 +0530

    Configure sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,31 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./context/userContext";
+import * as Sentry from "@sentry/react";
+
+
+//...
+
+Sentry.init({
+  dsn: "https://17be2cbcc0acc4a301da0aecff5420e6@o4505905599873024.ingest.sentry.io/4505905601576960",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <UserProvider>
