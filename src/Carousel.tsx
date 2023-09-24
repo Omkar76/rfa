67606f9dc5247f5fc7cbe558f0caa21cd777c659
Onkar Jadhav <omjadhav2610@gmail.com>
2023-09-25 01:51:39 +0530

    Replace icon buttons by text buttons

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -28,23 +28,16 @@ export const Carousel: FC<CarouselProps> = ({ children, setCurrent }) => {
   return (
     <div>
       <div className="flex items-center justify-between">
-        <svg
+        <button
+        className="bg-blue-600 text-white rounded-md p-2"
           style={{ visibility: current > 0 ? "visible" : "hidden" }}
-          onClick={() => dispatch("previous")}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <title>Previous</title>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
-          />
-        </svg>
+          onClick={(e) =>{
+            e.preventDefault(); 
+            dispatch("previous")}
+          }
+           >
+          Previous
+        </button>
 
         <div className="m-10 transition-all duration-1000 ease-in-out">
           {children.map((child, index) => {
@@ -59,25 +52,15 @@ export const Carousel: FC<CarouselProps> = ({ children, setCurrent }) => {
           })}
         </div>
 
-        <svg
+        <button
+        className="bg-blue-600 text-white rounded-md p-2"
           style={{
             visibility: current < children.length - 1 ? "visible" : "hidden",
           }}
-          onClick={() => dispatch("next")}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
+          onClick={(e) =>{e.preventDefault(); dispatch("next")}}
         >
-          <title>Next</title>
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
-          />
-        </svg>
+          Next
+        </button>
       </div>
     </div>
   );
